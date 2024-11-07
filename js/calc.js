cdcea5431c264899dd43782eadab4cbfbaa1891e
praveen <nairxprv@gmail.com>
2024-11-07 20:34:48 +0530

    added new files

---
+++ js/calc.js
@@ -0,0 +1,27 @@
+// import add from "./add.js";
+// import subtract from "./subtract.js"
+// let result = add(3,4)
+// console.log(result)
+// let result1 = subtract(6,3)
+// console.log(result1)
+
+export function add(x,y){
+    return x+y
+}
+
+export function subtract(x,y){
+    return x-y
+}
+
+
+export function multiple(x,y){
+    return x*y
+}
+
+
+export function divide(x,y){
+    return x/y
+}
+
+// export default add
+// export {add,subtract,multiple,divide}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+import {add,subtract,multiple,divide} from './calc.js'
+// import add from './calc.js'
+
+let result = add(4,5)
+console.log(result)
+
+let result1 = subtract(4,5)
+console.log(result1)
